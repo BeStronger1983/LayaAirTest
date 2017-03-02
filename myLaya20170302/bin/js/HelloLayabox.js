d9517ab0a6c5572c3ec7858c34681bbb7f3a3dd6
ponpon <wizardsoloist1983@gmail.com>
2017-03-02 17:53:29 +0800

    first commit

---
+++ myLaya20170302/bin/js/HelloLayabox.js
@@ -0,0 +1,17 @@
+var laya;
+(function (laya) {
+    var Text = Laya.Text;
+    var Sprite_Container = (function () {
+        function Sprite_Container() {
+            Laya.init(600, 300);
+            this.m_txt = new Text();
+            this.m_txt.text = "Hello Layabox";
+            this.m_txt.color = "#ffffff";
+            Laya.stage.addChild(this.m_txt);
+        }
+        return Sprite_Container;
+    }());
+    laya.Sprite_Container = Sprite_Container;
+})(laya || (laya = {}));
+new laya.Sprite_Container();
+//# sourceMappingURL=HelloLayabox.js.map
---
+++ myLaya20170302/src/HelloLayabox.ts
@@ -0,0 +1,17 @@
+module laya {
+    import Sprite = Laya.Sprite;
+    import Text = Laya.Text;
+
+    export class Sprite_Container {
+        private m_txt; 
+        constructor() {
+            Laya.init(600, 300);
+            this.m_txt = new Text();
+            this.m_txt.text = "Hello Layabox";
+            this.m_txt.color = "#ffffff";
+            Laya.stage.addChild(this.m_txt);
+        }
+    }
+}
+
+new laya.Sprite_Container();
